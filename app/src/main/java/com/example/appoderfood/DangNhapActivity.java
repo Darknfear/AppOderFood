607e1ae8d92560c9dd1639f77d4104a623b91700
nguye <darknfear1301@gmail.com>
2019-05-24 00:01:43 +0700

    Tạo màn hình đăng nhập

--- app/src/main/java/com/example/appoderfood/DangNhapActivity.java
+++ app/src/main/java/com/example/appoderfood/DangNhapActivity.java
@@ -1,13 +1,38 @@
 package com.example.appoderfood;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class DangNhapActivity extends AppCompatActivity {
+public class DangNhapActivity extends AppCompatActivity implements View.OnClickListener{
+
+    EditText edTenDangNhap2,edMatKhau2;
+    Button btnDangKy2,btnDangNhap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dang_nhap);
+
+        addControls();
+
+    }
+
+    private void addControls() {
+        edMatKhau2 = findViewById(R.id.edMạKhau2);
+        edTenDangNhap2 = findViewById(R.id.edTenDangNhap2);
+        btnDangKy2 = findViewById(R.id.btnDangKy2);
+        btnDangNhap = findViewById(R.id.btnDangNHap);
+
+        btnDangKy2.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(DangNhapActivity.this,DangKyActivity.class);
+        startActivity(intent);
     }
 }
